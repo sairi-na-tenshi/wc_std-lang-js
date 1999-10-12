dfed9fda7baa88be39cea24a788b1b81bfdb5187
sairi-na-tenshi <sairi.na.tenshi@gmail.com>
1999-10-12 19:36:03 +0400

    renaming

--- std-hlight-js.js
+++ std-hlight-js.js
@@ -1,6 +1,6 @@
 with( HLight ) lang.js= FPipe( FConcurentLang( new function(){
 
-	var wrapRemark= TagWrapper( 'i:hlight-js-remark' )
+	var wrapRemark= TagWrapper( 'std:hlight-js-remark' )
 	this[ '\\/\\*([\\s\\S]*?)\\*\\/' ]= function( content ){
 		content= lang.text( content )
 		return wrapRemark( '/*' + content + '*/' )
@@ -10,15 +10,15 @@ with( HLight ) lang.js= FPipe( FConcurentLang( new function(){
 		return wrapRemark( '//' + content )
 	}
 
-	var wrapString= TagWrapper( 'i:hlight-js-string' )
+	var wrapString= TagWrapper( 'std:hlight-js-string' )
 	this[ '"([^\\n]*?)"' ]= function( content ){
 		content= lang.text( content )
 		return wrapString( '"' + content + '"' )
 	}
 
-	var wrapKeyword= TagWrapper( 'i:hlight-js-keyword' )
+	var wrapKeyword= TagWrapper( 'std:hlight-js-keyword' )
 	this[ '\\b(this|function|new|var|if|else|switch|case|default|for|in|while|do|with|boolean|continue|break|throw|true|false|try|catch|null|typeof|instanceof|return|delete)\\b' ]= function( content ){
 		content= content || ''
 		return wrapKeyword( content )
 	}
-}), TagWrapper( 'i:hlight-js' ) )
+}), TagWrapper( 'std:hlight-js' ) )
